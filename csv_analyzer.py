f8ac1c4deda2571e4f5c9843db54ca191d396935
RiccardoPeron <riccardo.peron@studenti.unitn.it>
2022-05-03 14:05:18 +0200

    :sparkles: creates a plot with all the essentia features

---
+++ csv_analyzer.py
@@ -0,0 +1,31 @@
+import csv
+import pandas as pd
+import plotly.graph_objects as go
+from tqdm import tqdm
+
+df = pd.read_csv('results.csv', header=0)
+index = list(df.columns)
+
+tracks = index[0]
+
+x = [track[:5] for track in df[tracks]]
+
+fig = go.Figure()
+fig.update_layout(title='Essentia extractor plot',
+                  xaxis_title='song',
+                  yaxis_title='value')
+
+for id in tqdm(index):
+    if id != tracks and 'stdev' not in id:
+        y = [track for track in df[id]]
+        if not all(v == 0 for v in y):
+            fig.add_trace(
+                go.Scatter(
+                    x=x,
+                    y=y,
+                    name=id
+                )
+            )
+
+fig.write_html("file.html")
+fig.show()
