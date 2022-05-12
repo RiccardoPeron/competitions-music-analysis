30d568eb721e873a95263c3840f64ff0dd2aa008
RiccardoPeron <riccardo.peron@studenti.unitn.it>
2022-05-12 14:46:53 +0200

    :wrench: transformed downloader in a function

--- downloader.py
+++ downloader.py
@@ -1,3 +1,4 @@
+from spotdl import __main__ as spotdl
 import json
 import os
 import subprocess
@@ -5,34 +6,36 @@ import sys
 from pathlib import Path
 
 print('import spotdl...')
-from spotdl import __main__ as spotdl
 
 
-def download(title, link):
+def download(title, link, out_folder):
     print("downloading ", title, " OTS")
-    os.chdir('./OST')
+    os.chdir('./' + out_folder)
     os.mkdir(title)
     os.chdir('./' + title)
     # subprocess.check_call(['touch', 'a.txt'])
     # subprocess.check_call([sys.executable, spotdl.__file__, link])
-    subprocess.check_call(['spotdl', link])
+    subprocess.check_call(['spotdl', link, '--output-format wav'])
     os.chdir('..')
     os.chdir('..')
 
 
-print('open file...')
-file = open("movies.json")
-movies = json.load(file)
+def download_all(json_source, out_folder):
+    print('open file...')
+    file = open(json_source)
+    movies = json.load(file)
 
-print("creating main folder...")
-ost = Path('OST')
-if not ost.exists():
-    ost.mkdir()
+    print("creating main folder...")
+    ost = Path(out_folder)
+    if not ost.exists():
+        ost.mkdir()
 
-for movie in movies:
-    link = movie["link"].replace(" ", "_")
-    if movie["original_title"] == '':
+    for movie in movies:
+        link = movie["link"].replace(" ", "_")
         title = movie["title"].replace(" ", "_")
-    else:
-        title = movie["original_title"].replace(" ", "_")
-    download(title, link)
+        download(title, link, out_folder)
+
+    print('--- DONE ---')
+
+
+download_all('sanremo.json', 'Sanremo')
