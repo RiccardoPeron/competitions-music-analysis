28d1aeff2afce00b58b7f60dd1f4fba35ce2103d
RiccardoPeron <riccardo.peron@studenti.unitn.it>
2022-06-16 09:41:18 +0200

    :wrench: trasformed in a function

--- converter.py
+++ converter.py
@@ -5,46 +5,75 @@ from pathlib import Path
 from pydub import AudioSegment
 import eyed3
 
-# create the main folder
-ost = Path('OST_wav')
-if not ost.exists():
-    ost.mkdir()
 
-# iterate through the movies
-pbar = tqdm(sorted(os.listdir('OST')), desc='converting: ', ncols=100)
-for folder in pbar:
-    # create the movie folder in the main folder
-    if not Path('OST_wav/' + folder).exists():
-        Path('OST_wav/' + folder).mkdir()
+def check(source):
+    n_song = 0
+    n_wav_song = 0
+    for folder in os.listdir(source):
+        n_song += len(os.listdir(source + '/' + folder))
 
-    # iterate through the songs
-    for i, song in enumerate(sorted(os.listdir('OST/' + folder))):
-        if song != '.spotdl-cache':
-            # convert mp4 song
-            if song[-4:] == '.mp4':
-                video = AudioFileClip(os.path.join('OST', folder, song))
-                video.write_audiofile(
-                    os.path.join('OST_wav', folder, (song[:-4] + '.wav')))
-            # convert mp3 song
-            if song[-4:] == '.mp3':
-                # extract metadata
-                metadata = eyed3.load(os.path.join('OST', folder, song))
-                try:
-                    title = metadata.tag.title
-                    title = title.replace('/', '')
-                    track_n = metadata.tag.track_num[0]
-                except:
-                    title = song[:-4]
-                    track_n = i
-                # set all the track numbers in a NN format
-                if track_n < 10:
-                    track_n = '0'+str(track_n)
-                else:
-                    track_n = str(track_n)
+    for folder in os.listdir(source + '_wav'):
+        n_wav_song += len(os.listdir(source + '_wav/' + folder))
 
-                # convert into wav
-                pbar.set_postfix(f"| {title}")
-                sound = AudioSegment.from_mp3(
-                    os.path.join('OST', folder, song))
-                sound.export(os.path.join(
-                    'OST_wav', folder, (track_n + '-' + title + '.wav')), format='wav')
+    if(n_wav_song != n_song - len(os.listdir(source))):
+        print('>>>> WARNING: ' + str(n_wav_song) +
+              'is different from ' + str(n_song))
+
+
+def convert(source, usetrackn=True):
+    # create the main folder
+    ost = Path(source + '_wav')
+    if not ost.exists():
+        ost.mkdir()
+
+    n_song = 0
+    for folder in os.listdir(source):
+        n_song += len(os.listdir(source + '/' + folder))
+    pbar = tqdm(total=n_song, unit='files',
+                bar_format="Coverting:\t{percentage:.0f}%|{bar:100}{r_bar}")
+
+    # iterate through the movies
+    for folder in sorted(os.listdir(source)):
+        # create the movie folder in the main folder
+        if not Path(source + '_wav/' + folder).exists():
+            Path(source + '_wav/' + folder).mkdir()
+
+        # iterate through the songs
+        for i, song in enumerate(sorted(os.listdir(source + '/' + folder))):
+            if song != '.spotdl-cache':
+                # convert mp4 song
+                if song[-4:] == '.mp4':
+                    video = AudioFileClip(os.path.join(source, folder, song))
+                    video.write_audiofile(
+                        os.path.join(source + '_wav', folder, (song[:-4] + '.wav')))
+                # convert mp3 song
+                if song[-4:] == '.mp3':
+                    # extract metadata
+                    metadata = eyed3.load(os.path.join(source, folder, song))
+                    try:
+                        title = metadata.tag.title
+                        title = title.replace('/', '')
+                        track_n = metadata.tag.track_num[0]
+                    except:
+                        title = song[:-4]
+                    if not usetrackn:
+                        track_n = i
+                    # set all the track numbers in a NN format
+                    if track_n < 10:
+                        track_n = '0'+str(track_n)
+                    else:
+                        track_n = str(track_n)
+
+                    # convert into wav
+                    #pbar.set_postfix(f"| {title}")
+                    sound = AudioSegment.from_mp3(
+                        os.path.join(source, folder, song))
+                    sound.export(os.path.join(
+                        source + '_wav', folder, (track_n + '-' + title + '.wav')), format='wav')
+
+                    pbar.update(1)
+
+    check(source)
+
+
+# convert('Sanremo')
