2329b0fb6a7ef7d2baf2942288dfbe9813ebbcf7
RiccardoPeron <rcr1299@gmail.com>
2022-07-26 22:32:22 +0200

    :wrench: updated plot functions

--- Functions/plot.py
+++ Functions/plot.py
@@ -1,17 +1,23 @@
 import json
 from nis import cat
+from optparse import TitledHelpFormatter
+from os import sched_get_priority_max
+from typing import Counter
 
 import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-import json_to_lists as jlist
+from Functions import json_to_lists as jlist
+from collections import Counter
 
 
-def plot(schema, years, category):
+def plot(schema, years, category, title=""):
 
     fig = go.Figure()
-    fig.update_layout(title=category, xaxis_title="years", yaxis_title="features")
+    fig.update_layout(
+        title=category + " " + title, xaxis_title="years", yaxis_title="features"
+    )
 
     for element in schema[category]:
         y = schema[category][element]
@@ -19,6 +25,135 @@ def plot(schema, years, category):
 
     fig.show()
     return 0
+
+
+def plot_all(fname, source, opt="mean"):
+    schema, years = jlist.fill_schema(
+        fname,
+        y=opt,
+    )
+    title = "[" + source + ", " + opt + "]"
+    plot(schema, years, "time", title)
+    plot(schema, years, "tempo", title)
+    plot(schema, years, "tonal", title)
+
+
+def plot_keys(fname):
+    schema, years = jlist.fill_schema(
+        fname,
+        y="years",
+    )
+
+    keys = schema["tonal"]["key"]
+    keys_set = list(set(keys))
+    years_dict = Counter(years)
+
+    scat = go.Figure()
+    data = []
+    for key in keys_set:
+        index = 0
+        x = []
+        y = []
+        for year in years_dict:
+            x.append(year)
+            temp_keys = keys[index : index + years_dict[year]]
+            y.append(temp_keys.count(key))
+            index += years_dict[year] + 1
+        scat.add_trace(go.Scatter(name=key, x=x, y=y, mode="lines+markers"))
+        data.append(go.Bar(name=key, x=x, y=y))
+
+    fig = go.Figure(data)
+    fig.update_layout(title="key statsistics")
+    fig.update_layout(barmode="stack")
+    fig.show()
+    fig.update_layout(barmode="group")
+    fig.show()
+    scat.show()
+
+    return 0
+
+
+def plot_keys_pie(fname):
+    schema, years = jlist.fill_schema(
+        fname,
+        y="years",
+    )
+    keys = schema["tonal"]["key"]
+    maj_min = []
+
+    for key in keys:
+        if "major" in key:
+            maj_min.append("major")
+        else:
+            maj_min.append("minor")
+
+    k = Counter(keys)
+    maj_min = Counter(maj_min)
+
+    fig = make_subplots(
+        rows=1, cols=2, specs=[[{"type": "domain"}, {"type": "domain"}]]
+    )
+    fig.add_trace(go.Pie(labels=list(k.keys()), values=list(k.values())), 1, 1)
+    fig.add_trace(
+        go.Pie(labels=list(maj_min.keys()), values=list(maj_min.values())), 1, 2
+    )
+
+    fig.update_traces(hole=0.4, hoverinfo="label+percent+name")
+    fig.update_layout(
+        title_text="Songs keys",
+        # Add annotations in the center of the donut pies.
+        annotations=[
+            dict(text="Key distribucion", x=0.18, y=0.5, font_size=20, showarrow=False),
+            dict(text="Major/Minor", x=0.82, y=0.5, font_size=20, showarrow=False),
+        ],
+    )
+
+    fig.show()
+
+
+def plot_one_feature(schema, years, feature, title=""):
+    fig = go.Figure()
+    fig.update_layout(
+        title=feature + " " + title, xaxis_title="years", yaxis_title="features"
+    )
+
+    for category in schema:
+        for element in schema[category]:
+            if element == feature:
+                y = schema[category][element]
+                fig.add_trace(
+                    go.Scatter(x=years, y=y, mode="lines+markers", name=element)
+                )
+
+    fig.show()
+    return 0
+
+
+# EXPERIMENTAL
+
+
+def plot_multi(feature, opt):
+
+    fig = go.Figure()
+    fig.update_layout(
+        title=feature + " " + opt, xaxis_title="years", yaxis_title="features"
+    )
+
+    for competition in ["ESC", "Sanremo"]:
+        fname = "Results/" + competition + "songs_essentia.json"
+        schema, years = jlist.fill_schema(fname, opt)
+        for category in schema:
+            for element in schema[category]:
+                if element == feature:
+                    y = schema[category][element]
+                    fig.add_trace(
+                        go.Scatter(x=years, y=y, mode="lines+markers", name=competition)
+                    )
+
+    fig.show()
+
+
+# OLD BELOW
 
 
 def plot_bpm(fname):
@@ -117,51 +252,3 @@ def compare_albums_bpm(fname, field):
         )
 
     fig.show()
-
-
-def plot_keys(fname):
-    f = open(fname)
-    data = json.load(f)
-
-    k = []
-    maj_min = []
-
-    for album in data:
-        for song in album["songs"]:
-            k.append(song["key"])
-            if "major" in song["key"]:
-                maj_min.append("major")
-            else:
-                maj_min.append("minor")
-
-    k = Counter(k)
-    maj_min = Counter(maj_min)
-
-    fig = make_subplots(
-        rows=1, cols=2, specs=[[{"type": "domain"}, {"type": "domain"}]]
-    )
-    fig.add_trace(go.Pie(labels=list(k.keys()), values=list(k.values())), 1, 1)
-    fig.add_trace(
-        go.Pie(labels=list(maj_min.keys()), values=list(maj_min.values())), 1, 2
-    )
-
-    fig.update_traces(hole=0.4, hoverinfo="label+percent+name")
-    fig.update_layout(
-        title_text="Songs keys",
-        # Add annotations in the center of the donut pies.
-        annotations=[
-            dict(text="Key distribucion", x=0.18, y=0.5, font_size=20, showarrow=False),
-            dict(text="Major/Minor", x=0.82, y=0.5, font_size=20, showarrow=False),
-        ],
-    )
-
-    fig.show()
-
-
-schema, years = jlist.fill_schema(
-    "/home/riccardo/Tesi/disney-ost-analysis/Results/Sanremosongs_essentia.json",
-    y="mean",
-)
-plot(schema, years, "time")
-plot(schema, years, "tempo")
-plot(schema, years, "tonal")
