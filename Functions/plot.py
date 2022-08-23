72c17808d6ff800046f91cfe6fadc9ce4b06ec24
RiccardoPeron <rcr1299@gmail.com>
2022-08-23 11:36:13 +0200

    :wastebasket: removed old code

--- Functions/plot.py
+++ Functions/plot.py
@@ -50,6 +50,7 @@ def plot_keys(fname):
 
     scat = go.Figure()
     data = []
+
     for key in keys_set:
         index = 0
         x = []
@@ -57,10 +58,20 @@ def plot_keys(fname):
         for year in years_dict:
             x.append(year)
             temp_keys = keys[index : index + years_dict[year]]
-            y.append(temp_keys.count(key))
+            y.append(temp_keys.count(key) * 100 / years_dict[year])
             index += years_dict[year] + 1
-        scat.add_trace(go.Scatter(name=key, x=x, y=y, mode="lines+markers"))
         data.append(go.Bar(name=key, x=x, y=y))
+
+    scat_x = []
+    scat_y = []
+
+    for year in years_dict:
+        scat_x.append(years_dict)
+        for key in keys_set:
+            temp_keys = set(keys[index : index + years_dict[year]])
+            scat_y.append(len(temp_keys))
+
+    scat.add_trace(go.Scatter(name=key, x=scat_x, y=scat_y, mode="lines+markers"))
 
     fig = go.Figure(data)
     fig.update_layout(title="key statsistics")
@@ -149,106 +160,5 @@ def plot_multi(feature, opt):
                     fig.add_trace(
                         go.Scatter(x=years, y=y, mode="lines+markers", name=competition)
                     )
-
-    fig.show()
-
-
-# OLD BELOW
-
-
-def plot_bpm(fname):
-    f = open(fname)
-    data = json.load(f)
-
-    x = []
-    y = []
-    y_60 = []
-
-    for album in data:
-        for song in album["songs"]:
-            if song["title"] not in x:
-                x.append(song["title"])
-            else:
-                x.append(
-                    song["title"]
-                    + "_"
-                    + str(
-                        x.count(song["title"])
-                        + sum(
-                            [
-                                x.count(song["title"] + "_" + str(y))
-                                for y in [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                            ]
-                        )
-                    )
-                )
-            y.append(song["bpm"])
-            y_60.append(song["bpm_60"])
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=y, mode="lines+markers", name="full song"))
-    fig.add_trace(
-        go.Scatter(x=x, y=y_60, mode="lines+markers", name="first 60 seconds")
-    )
-
-    fig.update_layout(title="Songs BPMs", xaxis_title="song title", yaxis_title="BPMs")
-    print(len(x), len(set(x)))
-    fig.show()
-
-
-def plot_average_bpm(fname):
-    f = open(fname)
-    data = json.load(f)
-
-    x = []
-    y = []
-    y_60 = []
-
-    for album in data:
-        tmpy = []
-        tmpy_60 = []
-        for song in album["songs"]:
-            tmpy.append(song["bpm"])
-            tmpy_60.append(song["bpm_60"])
-        x.append(album["title"])
-        y.append(sum(tmpy) / len(tmpy))
-        y_60.append(sum(tmpy_60) / len(tmpy_60))
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=y, mode="lines+markers", name="full song"))
-    fig.add_trace(
-        go.Scatter(x=x, y=y_60, mode="lines+markers", name="first 60 seconds")
-    )
-
-    fig.update_layout(
-        title="album average BPMs", xaxis_title="song title", yaxis_title="BPMs"
-    )
-
-    fig.show()
-
-
-def compare_albums_bpm(fname, field):
-    f = open(fname)
-    data = json.load(f)
-    fig = go.Figure()
-    fig.update_layout(
-        title="album " + field + " comparison",
-        xaxis_title="song number",
-        yaxis_title="BPM",
-    )
-
-    for album in data:
-        song_n = []
-        song_bpm = []
-        for i, song in enumerate(album["songs"]):
-            song_n.append(i)
-            song_bpm.append(song[field])
-
-        k = 100 // len(song_n)
-        song_n = [i * k for i in range(len(song_n))]
-
-        fig.add_trace(
-            go.Scatter(x=song_n, y=song_bpm, mode="lines", name=album["title"])
-        )
 
     fig.show()
