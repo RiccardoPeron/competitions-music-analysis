ddd0d26f40f43fac290c46caf480b93014354b2a
RiccardoPeron <rcr1299@gmail.com>
2022-08-23 11:32:28 +0200

    :sparkles: added pvalues

--- Functions/evaluator.py
+++ Functions/evaluator.py
@@ -5,6 +5,7 @@ import pandas as pd
 from tqdm import tqdm
 
 from Functions import json_to_lists as jlist
+from scipy.stats import pearsonr
 
 
 def get_coefficent(fname, metric):
@@ -21,8 +22,8 @@ def get_coefficent(fname, metric):
         # x.append(mean(m))
         x.append(album["year"])
 
-    coeff = np.corrcoef(x, y)
-    return coeff[0][1]
+    coeff, pvalue = pearsonr(x, y)
+    return coeff, pvalue
 
 
 def get_csv_coefficent(fname, csv):
@@ -63,31 +64,39 @@ def get_csv_coefficent(fname, csv):
 
 def get_all_coeff(schema, years):
     coeff_list = []
+    pvalue_list = []
     for category in ["time", "tempo", "tonal"]:
         for element in schema[category]:
             try:
                 metric = schema[category][element]
-                coeff = np.corrcoef(metric, years)
-                coeff = float("{:.2f}".format(coeff[0][1]))
+                coeff, pvalue = pearsonr(metric, years)  # np.corrcoef(metric, years)
+                coeff = float("{:.2f}".format(coeff))
+                pvalue = float("{:.2f}".format(pvalue))
+
                 coeff_list.append(coeff)
+                pvalue_list.append(pvalue)
                 # print(f"| {element}\t\t\t\t|\t{coeff} |")
             except:
                 # print(f"| {element}\t\t\t\t|\tTOFIX |")
                 coeff_list.append("TOFIX")
+                pvalue_list.append("TOFIX")
                 len(metric) == len(years)
-    return coeff_list
+    return coeff_list, pvalue_list
 
 
 def make_latex_table(fname):
     options = ["years", "index", "mean"]
     coeffs = []
+    pvalues = []
 
     for opt in options:
         schema, years = jlist.fill_schema(
             fname,
             y=opt,
         )
-        coeffs.append(get_all_coeff(schema, years))
+        coeff, pvalue = get_all_coeff(schema, years)
+        coeffs.append(coeff)
+        pvalues.append(pvalue)
 
     table = (
         "\\begin{table}[!ht]\n"
@@ -96,10 +105,12 @@ def make_latex_table(fname):
         + "\\toprule\n"
         + "{\color[HTML]{333333} \\textbf{Feature}} & {\color[HTML]{333333} \\textbf{Year PRC}} & {\color[HTML]{333333} \\textbf{Song index PRC}} & {\color[HTML]{333333} \\textbf{Year mean PRC}} \\\\ \midrule\n"
     )
+    ptable = table
     i = 0
     for metric in jlist.metrics:
         if metric == "\hline":
             table += metric + "\n"
+            ptable += metric + "\n"
         else:
             table += (
                 metric
@@ -111,6 +122,16 @@ def make_latex_table(fname):
                 + str(coeffs[2][i])
                 + "\\\\\n"
             )
+            ptable += (
+                metric
+                + "&"
+                + str(pvalues[0][i])
+                + "&"
+                + str(pvalues[1][i])
+                + "&"
+                + str(pvalues[2][i])
+                + "\\\\\n"
+            )
             i += 1
 
     table += (
@@ -119,8 +140,14 @@ def make_latex_table(fname):
         + "\label{table:}\n"
         + "\end{table}"
     )
+    ptable += (
+        "\end{tabular}\n"
+        + "\caption{Pearson Relation Coefficent relativo ad ogni metrica}\n"
+        + "\label{table:}\n"
+        + "\end{table}"
+    )
 
-    return table
+    return table, ptable
 
 
 # schema, years = jlist.fill_schema(
